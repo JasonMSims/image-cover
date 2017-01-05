3f093064cc4e38717b9741f8d236f0a02cfa1a74
Jason Sims <jason.sims@ncr.com>
2017-01-05 13:37:46 -0500

    Refactored the initialization and loops

--- js/image-cover.js
+++ js/image-cover.js
@@ -1,46 +1,64 @@
 ;(function($) {
 
-      $.fn.cover = function(options) {
+  $.fn.cover = function(options) {
 
-        o = $.extend({
-          target: 'img',
-        }, options);
+    var defaults = {
+      target: 'img',
+      delay: 100
+    };
 
-        return this.each(function() {
-          var container = $(this);
-          var image = container.children(o.target);
+    o = $.extend(defaults, options);
 
-          var position = function() {
-            var containerRatio = (container.width() / container.height());
-            var imageRatio = (image.width() / image.height());
-            if (containerRatio < imageRatio) { // The image is wider than the container
-              image.css({ // height to 100% and left margin offset
-                height: '100%',
-                width: 'auto'
-              });
-              image.css({
-                marginLeft: '-' + ((image.width() - container.width()) / 2) + 'px',
-                marginTop: 0
-              });
-            }
-            else { // The image is not as wide as the container
-              image.css({ // width to 100% and top margin offset
-                height: 'auto',
-                width: '100%'
-              }),
-              image.css({ // width to 100% and top margin offset
-                marginLeft: 0,
-                marginTop: '-' + ((image.height() - container.height()) / 2) + 'px'
-              })
-            }
-          };
 
-          position();
+    var container = $(this);
+    var images = container.children(o.target);
+    var timer;
 
-          $(window).resize(function() {
-            position();
+    function position(image) {
+        var containerRatio = (container.width() / container.height());
+        var imageRatio = (image.width() / image.height());
+        if (containerRatio < imageRatio) { // the image is wider than the container
+          image.css({ // height to 100% and left margin offset
+            height: '100%',
+            width: 'auto'
           });
-        });
-      };
+          image.css({
+            marginLeft: '-' + ((image.width() - container.width()) / 2) + 'px',
+            marginTop: 0
+          });
+        }
+        else { // the image is not as wide as the container
+          image.css({ // width to 100% and top margin offset
+            height: 'auto',
+            width: '100%'
+          }),
+          image.css({ // width to 100% and top margin offset
+            marginLeft: 0,
+            marginTop: '-' + ((image.height() - container.height()) / 2) + 'px'
+          });
+        }
+      }
 
-    }(jQuery, window, document));
+    $(window).on('resize', function() {
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        images.each(function() {
+          position($(this));
+        });
+      }, o.delay);
+    });
+
+    return images.each(function() {
+      var image = $(this);
+
+      image.load(function() { // when the image is loaded, run the position function
+        position(image);
+      });
+
+      if (this.complete) { // if the image is cached, run the position function
+        position(image);
+      }
+    });
+  };
+
+}(jQuery, window, document));
