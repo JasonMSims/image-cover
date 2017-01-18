ecc4025e883d103baad7f614f678fbf2aae649dd
Sims <js185455@wusjs185455-tio.corp.ncr.com>
2017-01-18 13:10:36 -0500

    Added more scale conditions. Began work on breakpoints.

--- js/image-cover.js
+++ js/image-cover.js
@@ -4,7 +4,9 @@
 
     var defaults = {
       target: 'img',
-      delay: 100
+      delay: 100,
+      scale: 'fill',
+      responsive: []
     };
 
     var o = $.extend(defaults, options);
@@ -12,13 +14,57 @@
     var container = $(this);
     var images = container.find(o.target);
     var timer;
+    var scale = o.scale;
 
     function position(image) {
+
+        if (o.responsive.length) {
+          $(o.responsive).each(function(index, obj) {
+            if (obj.breakpoint !== undefined) {
+              if ($(window).width() <= obj.breakpoint) {
+                if(obj.settings !== undefined) {
+                  $.each(obj.settings, function(key, value) {
+                    scale = value;
+                  });
+                }
+              }
+              else {
+                scale = o.scale;
+              }
+            }
+          });
+        }
+
         var containerRatio = (container.width() / container.height());
         var imageRatio = (image.width() / image.height());
-        if (containerRatio < imageRatio) { // the image is wider than the container
+        var fillHeight = container.height();
+        var fillWidth = container.width();
+
+        if (scale === 'fill') {
+          if (containerRatio < imageRatio) { // the image is wider than the container
+            image.css({ // height to 100% and left margin offset
+              height: fillHeight,
+              width: 'auto'
+            });
+            image.css({
+              marginLeft: '-' + ((image.width() - container.width()) / 2) + 'px',
+              marginTop: 0
+            });
+          }
+          else { // the image is not as wide as the container
+            image.css({ // width to 100% and top margin offset
+              height: 'auto',
+              width: fillWidth
+            });
+            image.css({ // width to 100% and top margin offset
+              marginLeft: 0,
+              marginTop: '-' + ((image.height() - container.height()) / 2) + 'px'
+            });
+          }
+        }
+        else if (scale === 'fill-height') {
           image.css({ // height to 100% and left margin offset
-            height: '100%',
+            height: fillHeight,
             width: 'auto'
           });
           image.css({
@@ -26,15 +72,19 @@
             marginTop: 0
           });
         }
-        else { // the image is not as wide as the container
+        else if (scale === 'fill-width') {
           image.css({ // width to 100% and top margin offset
             height: 'auto',
-            width: '100%'
+            width: fillWidth
           });
           image.css({ // width to 100% and top margin offset
             marginLeft: 0,
-            marginTop: '-' + ((image.height() - container.height()) / 2) + 'px'
+            marginTop: 0
           });
+        }
+        else {
+          scale = 'fill';
+          position(image);
         }
       }
 
@@ -48,6 +98,7 @@
     });
 
     return images.each(function() {
+
       var image = $(this);
 
       image.load(function() { // when the image is loaded, run the position function
